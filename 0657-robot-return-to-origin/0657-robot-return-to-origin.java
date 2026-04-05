e3b073192160bee7e6519e3484318f5591d69909
Shikha Singh <121444818+Shikhaxingh01@users.noreply.github.com>
2026-04-05 23:44:53 +0530

    Time: 4 ms (99.05%), Space: 45.9 MB (48.28%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int up = 0, down = 0, left = 0, right = 0;
+
+        for (char ch : moves.toCharArray()) {
+            if (ch == 'U') up++;
+            else if (ch == 'D') down++;
+            else if (ch == 'L') left++;
+            else if (ch == 'R') right++;
+        }
+
+        return up == down && left == right;
+    }
+}
